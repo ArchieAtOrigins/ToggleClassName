972eff58bf4860bb0619e3df2c21cbbb076743fb
ArchieAtOrigins <archie@originscreative.com>
2017-02-03 15:51:43 -0800

    Inital commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var theBear = document.getElementById('bear');
+
+
+var changeUp = function() {
+	if (theBear.className = "yogi") {
+		theBear.className = "booboo";
+	} else {
+		theBear.className = "yogi";
+	}
+
+}
+
+document.getElementById('change').addEventListener('click', changeUp);
