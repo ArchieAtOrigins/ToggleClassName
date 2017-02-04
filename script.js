1b111dea8ad527acfd1a9d731cb845edab368209
ArchieAtOrigins <archie@originscreative.com>
2017-02-03 16:26:30 -0800

    Changed single = in conditional to double == and now it toggles both ways

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var theBear = document.getElementById('bear');
 
 
 var changeUp = function() {
-	if (theBear.className = "yogi") {
+	if (theBear.className == "yogi") {
 		theBear.className = "booboo";
 	} else {
 		theBear.className = "yogi";
